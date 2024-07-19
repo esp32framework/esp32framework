b8a84598043d0ea86d49abbafc447ed1f3152971
DiegoCivi <dcivini@fi.uba.ar>
2024-07-19 20:22:31 -0300

    Add example for using the DS3231 as a cronometer with I2C communication
    Co-authored-by: mateogdupont <mateogdupont@users.noreply.github.com>
    Co-authored-by: Joaquin <jorivero@fi.uba.ar>
    Co-authored-by: Juan Pablo Aschieri <higlak@users.noreply.github.com>

---
+++ examples/ds3231_cronometer.rs
@@ -0,0 +1,44 @@
+use esp_idf_svc::hal::delay::{FreeRtos, BLOCK};
+use esp_idf_svc::hal::i2c::*;
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::hal::prelude::*;
+
+use std::str;
+
+const ADDRESS: u8 = 0x68;
+
+
+fn bcd_to_decimal(bcd: u8) -> u8 {
+    (bcd & 0x0F) + ((bcd >> 4) * 10)
+}
+
+fn main() {
+    esp_idf_svc::sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let i2c = peripherals.i2c0;
+    let sda = peripherals.pins.gpio5;
+    let scl = peripherals.pins.gpio6;
+
+    let config = I2cConfig::new().baudrate(100.kHz().into());
+    let mut driver = I2cDriver::new(i2c, sda, scl, &config).unwrap();
+    let mut buffer = [0u8; 19];
+
+    loop {
+        driver.read(ADDRESS, &mut buffer, 50);
+        let seconds = bcd_to_decimal(buffer[0]);
+        let minutes = bcd_to_decimal(buffer[1]);
+        let hours = bcd_to_decimal(buffer[2] & 0x3F);
+        let day_of_week = bcd_to_decimal(buffer[3]);
+        let day_of_month = bcd_to_decimal(buffer[4]);
+        let month = bcd_to_decimal(buffer[5] & 0x1F);
+        let year = bcd_to_decimal(buffer[6]) as u32 + 2000;
+
+        println!("Raw data: {:?}",buffer);
+
+        println!("Parsed: {:?}/{:?}/{:?} {:?}:{:?}:{:?}", day_of_week,month,year,hours,minutes,seconds);
+        
+        FreeRtos::delay_ms(500);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,134 +1,81 @@
+//! I2C test with SSD1306
+//!
+//! Folowing pins are used:
+//! SDA     GPIO5
+//! SCL     GPIO6
+//!
+//! Depending on your target and the board you are using you have to change the pins.
+//!
+//! For this example you need to hook up an SSD1306 I2C display.
+//! The display will flash black and white.
 
-/*
-use std::thread;
-use std::time::Duration;
-use esp_idf_svc::hal::adc::config::Config;
-use esp_idf_svc::hal::adc::*;
-use esp_idf_svc::hal::delay::FreeRtos;
-use esp_idf_svc::hal::gpio::*;
-use esp_idf_svc::hal::peripherals;
-use esp_idf_svc::hal::peripherals::Peripherals;
-use microcontroller::Microcontroller;
-use esp_idf_svc::hal::ledc::*;
+use esp_idf_svc::hal::delay::{FreeRtos, BLOCK};
+use esp_idf_svc::hal::i2c::*;
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::hal::prelude::*;
-*/
-mod gpio;
-mod utils;
-mod microcontroller;
-use crate::microcontroller::microcontroller::Microcontroller;
-use crate::gpio::{InterruptType, DigitalIn};
 
-use esp_idf_svc::hal::delay::FreeRtos;
+use std::str;
+
+const ADDRESS: u8 = 0x68;
 
 
-/*
-fn main(){
-    let mut micro = Microcontroller::new();
-    println!("Configuring output channel");
-    
-    let frec = 10;
-
-    // Set ledC to create a PWM signal
-
-    let peripherals = Peripherals::take().unwrap();
-    let mut channel = LedcDriver::new(
-        peripherals.ledc.channel0,
-        LedcTimerDriver::new(
-            peripherals.ledc.timer0,
-            &config::TimerConfig::new().frequency((100).kHz().into()).resolution(Resolution::Bits5),
-        ).unwrap(),
-        peripherals.pins.gpio4,
-    ).unwrap();
-
-    let digital_in = micro.set_pin_as_digital_in(5, InterruptType::PosEdge);
-    
-    println!("Starting duty-cycle loop");
-
-    let max_duty = channel.get_max_duty();
-    for numerator in [0, 1, 2, 3, 4, 5].iter().cycle() {
-        println!("Duty {numerator}/5");
-        channel.set_duty(max_duty * numerator / 5).unwrap();
-        
-        for i in 0..3{
-            let second_method = second_read_method(frec, &digital_in, numerator);
-            let first_method = first_read_method(2* frec * 1000, &digital_in, numerator);
-            println!("Percentage sent {}, on read {}:  percentage 1st method: {} %   |   percentage 2nd method: {} %", numerator, i, first_method, second_method);
-        }
-
-        FreeRtos::delay_ms(500);
-    }
-
-    loop {
-        FreeRtos::delay_ms(1000);
-    }
-}*/
-
-
-fn main(){
-    let mut micro = Microcontroller::new();
-    println!("Configuring output channel");
-    
-    let frec = 10;
-    let pin = 4; 
-    let resolution = 12;
-    let mut analog_out = micro.set_pin_as_analog_out(pin, frec * 1000, resolution);
-    
-    let pin_num = 5;
-    let analog_in_pwm = micro.set_pin_as_analog_in_pwm(pin_num, frec * 1000);
-    let mut adc_analog_in = micro.set_pin_as_analog_in_high_atten(6);
-
-    
-    println!("Starting duty-cycle loop");
-
-    for ratio in [0.0, 0.2, 0.4, 0.6, 0.8, 1.0].iter().cycle() {
-        println!("Duty {ratio}");
-        analog_out.set_high_level_output_ratio(*ratio as f32).unwrap();
-        
-        for i in 0..3 {
-            let read_val = analog_in_pwm.read();
-            let read_adc_val = adc_analog_in.read().unwrap();
-            let read_raw_adc_val = adc_analog_in.read_raw().unwrap();
-            println!("Read1 {} | Reas Raw {}", read_adc_val, read_raw_adc_val);  
-            println!("Percentage sent {}, on read {}:  percentage received: {} %", ratio, i, read_val);
-        }
-        FreeRtos::delay_ms(500);
-    }
-
-    loop {
-        FreeRtos::delay_ms(1000);
-    }
+fn bcd_to_decimal(bcd: u8) -> u8 {
+    (bcd & 0x0F) + ((bcd >> 4) * 10)
 }
 
-// fn second_read_method(frec: u32, digital_in: &DigitalIn)-> f32{
-//     let mut reads = 0.0;
-//     let amount_of_reads = 100;
-//     for _i in 0..amount_of_reads{
-//         reads += first_read_method(2* frec, digital_in)
-//     }
-//     return reads / (amount_of_reads as f32)
-// }
-
-// fn first_read_method(reading: u32, digital_in: &DigitalIn)-> f32{
-//     let mut highs = 0;
-//     for _num in 0..(reading){
-//         if digital_in.is_high(){
-//             highs += 1
-//         }
-//     } 
-//     let a: f32 = (highs as f32) / (reading as f32);
-    
-//     return a
+// fn read_values -> dict{
+//     driver.read();
+//     parse...
+//     return [hour: '',minu:]
 // }
 
 
+fn main() {
+    esp_idf_svc::sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let i2c = peripherals.i2c0;
+    let sda = peripherals.pins.gpio5;
+    let scl = peripherals.pins.gpio6;
+
+    let config = I2cConfig::new().baudrate(100.kHz().into());
+    let mut driver = I2cDriver::new(i2c, sda, scl, &config).unwrap();
+    let mut buffer = [0u8; 19];
+    let mut data: [u8; 8] = [0; 8];
+
+    // ejecutar_fuera_de_rango(hour,0 , 10, fn prender_luz)
+
+    data[0] = (0 / 10 * 16) + (0 % 10);
+    data[1] = (10 / 10 * 16) + (10 % 10);
+    data[2] = (1 / 10 * 16) + (1 % 10);
+    data[3] = 5;
+    data[4] = (19 / 10 * 16) + (19 % 10);
+    data[5] = (7 / 10 * 16) + (7 % 10);
+    data[6] = ((2024 / 10 * 16) + (2024 % 10)) as u8;
+
+    // Control register (not setting CH bit to enable oscillator)
+    data[7] = 0x00;
+
+    // Escribir los datos en el DS3231
+    driver.write(ADDRESS, &data, 10).unwrap();
 
 
+    loop {
+        driver.read(ADDRESS, &mut buffer, 50);
+        //let read_val = str::from_utf8(&buffer).unwrap();
+        let seconds = bcd_to_decimal(buffer[0]);
+        let minutes = bcd_to_decimal(buffer[1]);
+        let hours = bcd_to_decimal(buffer[2] & 0x3F); // Ignorar el bit de formato 12/24 horas
+        let day_of_week = bcd_to_decimal(buffer[3]);
+        let day_of_month = bcd_to_decimal(buffer[4]);
+        let month = bcd_to_decimal(buffer[5] & 0x1F); // Ignorar el bit del siglo
+        let year = bcd_to_decimal(buffer[6]) as u32 + 2000; // Asumir siglo 21
 
-// 2,33333333333333333333333333333333987139847932847932478
+        println!("La raw es: {:?} y pasado a str es",buffer);
 
-// 2,3333333
+        println!("{:?}/{:?}/{:?} {:?}:{:?}:{:?}", day_of_week,month,year,hours,minutes,seconds);
+        
+        FreeRtos::delay_ms(500);
+    }
 
-// [0; 3,3]    2**6.5 
-
-// [0; 3,3] 
+}
