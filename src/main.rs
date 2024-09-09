20f42a114964e67087f1dc4ebfedb6a31aa888eb
DiegoCivi <dcivini@fi.uba.ar>
2024-09-09 15:13:26 -0300

    Fixing microcontroller documentation. Update related functions are still missing documentation

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,74 @@
 use esp32framework::{Microcontroller, ble::{ble_client::BleClient, BleId}};
-use esp32_nimble::BLEDevice;
-fn main(){
-    let mut micro = Microcontroller::new();
-    
-    let ble_device = BLEDevice::take();
-    let mut client = BleClient::new(ble_device);
-    client.connect_to_device_of_name(None, "Galaxy S10e".to_string()).unwrap();
-    
-    println!("Connected");
-    micro.wait_for_updates(Some(2000));
-    client.prueba();
-    micro.wait_for_updates(None)
-}
-/*
+use esp32_nimble::{BLEAdvertisementData, BLEDevice, DescriptorProperties, NimbleProperties};
+// fn main(){
+//     let mut micro = Microcontroller::new();
+	
+//     let ble_device = BLEDevice::take();
+//     let mut client = BleClient::new(ble_device);
+//     client.connect_to_device_of_name(None, "Galaxy S10e".to_string()).unwrap();
+	
+//     println!("Connected");
+//     micro.wait_for_updates(Some(2000));
+//     client.prueba();
+//     micro.wait_for_updates(None)
+// }
+
+
 use bstr::ByteSlice;
-use esp32_nimble::{utilities::BleUuid, uuid128, BLEClient, BLEDevice};
+use esp32_nimble::{utilities::BleUuid, uuid128, BLEClient};
 use esp_idf_svc::hal::{
   delay::FreeRtos, prelude::Peripherals, task::block_on, timer::{TimerConfig, TimerDriver}
 };
 
 fn main() {
-  esp_idf_svc::sys::link_patches();
-  esp_idf_svc::log::EspLogger::initialize_default();
+	esp_idf_svc::sys::link_patches();
+	esp_idf_svc::log::EspLogger::initialize_default();
+  
+	let device = BLEDevice::take();
+	let ble_advertising = device.get_advertising();
+  
+	let server = device.get_server();
+	server.on_connect(|server, desc| {
+	  ::log::info!("Client connected: {:?}", desc);
+  
+		if server.connected_count() < (esp_idf_svc::sys::CONFIG_BT_NIMBLE_MAX_CONNECTIONS as _) {
+			::log::info!("Multi-connect support: start advertising");
+			ble_advertising.lock().start().unwrap();
+		}
+	});
+	server.on_disconnect(|_desc, reason| {
+	  ::log::info!("Client disconnected ({:?})", reason);
+	  if let Err(e) = reason {
+		  println!("El error fue {:?}", e.to_string());
+	  }
+	});
+  
+	let service = server.create_service(BleUuid::Uuid16(0xABCD));
+  
+	let characteristic = service
+	  .lock()
+	  .create_characteristic(BleUuid::Uuid16(0xAAAA), NimbleProperties::READ);
+	characteristic
+	  .lock()
+	  .set_value("non_secure_characteristic".as_bytes());
 
-  let peripherals = Peripherals::take().unwrap();
-  let mut timer = TimerDriver::new(peripherals.timer00, &TimerConfig::new()).unwrap();
-
-  block_on(async {
-    let ble_device = BLEDevice::take();
-    let ble_scan = ble_device.get_scan();
-    let device = ble_scan
-      .active_scan(true)
-      .interval(100)
-      .window(99)
-      .find_device(10000, |device| {
-          println!("device_name: {}", device.name());
-          device.name().contains_str("e")
-        })
-      .await.unwrap();
-
-    if let Some(device) = device {
-      let mut client = BLEClient::new();
-      client.on_connect(|client| {
-        client.update_conn_params(120, 120, 0, 60).unwrap();
-      });
-      client.connect(device.addr()).await.unwrap();
-
-      for s in client.get_services().await.unwrap(){
-        println!("service: {s}");
-      }
-      
-      let service = client
-        .get_service(BleUuid::Uuid16(0x1515))
-        .await.unwrap();
-
-      let uuid = BleUuid::Uuid16(0x0101);
-      let characteristic = service.get_characteristic(uuid).await.unwrap();
-      let value = characteristic.read_value().await.unwrap();
-      println!("{characteristic} tiene value: {:?}", value);
-      
-      let uuid = BleUuid::Uuid16(0x0202);
-      let characteristic = service.get_characteristic(uuid).await.unwrap();
-
-      if !characteristic.can_notify() {
-        ::log::error!("characteristic can't notify: {}", characteristic);
-      }
-
-      println!("subscribe to {}", characteristic);
-      characteristic
-        .on_notify(move |data| {
-        println!("{uuid} notified: {:?}", data)
-        })
-        .subscribe_notify(false)
-        .await.unwrap();
-      timer.delay(timer.tick_hz() * 50).await.unwrap();
-
-      client.disconnect().unwrap();
-    }
-  });
-  FreeRtos::delay_ms(5000)
+	let desc = characteristic.lock().create_descriptor(BleUuid::Uuid16(0x11), DescriptorProperties::READ);
+	desc.lock().set_value(&[0x12;1]);
+  
+	// With esp32-c3, advertising stops when a device is bonded.
+	// (https://github.com/taks/esp32-nimble/issues/70)
+	ble_advertising.lock().set_data(
+	  BLEAdvertisementData::new()
+		.name("ESP32-GATT-Server")
+		.add_service_uuid(BleUuid::Uuid16(0xABCD)),
+	).unwrap();
+	ble_advertising.lock().start().unwrap();
+  
+	::log::info!("bonded_addresses: {:?}", device.bonded_addresses());
+  
+	loop {
+	  esp_idf_svc::hal::delay::FreeRtos::delay_ms(1000);
+	}
 }
 
- */
+
