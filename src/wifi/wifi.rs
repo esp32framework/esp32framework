b5fd8ee94e48d40ad57fb906b9783139f00728b2
DiegoCivi <dcivini@fi.uba.ar>
2024-09-24 21:03:53 -0300

    Add docu to wifi
    Co-authored-by: mateogdupont <mateogdupont@users.noreply.github.com>

--- src/wifi/wifi.rs
+++ src/wifi/wifi.rs
@@ -1,9 +1,8 @@
 use std::net::Ipv4Addr;
-
 use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::modem::{self}, nvs::EspDefaultNvsPartition, timer::EspTaskTimerService, wifi::{AsyncWifi, AuthMethod, ClientConfiguration, Configuration, EspWifi}};
-
 use super::http::{Http, HttpClient, HttpsClient};
 
+/// Error types related to WIFI operations.
 #[derive(Debug)]
 pub enum WifiError {
     ConfigurationError,
@@ -15,6 +14,8 @@ pub enum WifiError {
     HttpError
 }
 
+/// Abstraction of the driver that controls the wifi. It simplifies
+/// the wifi connection and the creation of an HTTP client.
 pub struct WifiDriver<'a> {
     controller: AsyncWifi<EspWifi<'a>>,
 }
@@ -35,6 +36,25 @@ impl <'a>WifiDriver<'a> {
         }
     }
 
+    /// Connects to the desired wifi network. 
+    /// 
+    /// If a password is passed, it connects using the WPAWPA2Personal Authentication method.
+    /// Otherwise, it doesn't use an Authentication method.
+    /// 
+    /// # Arguments
+    /// 
+    /// - `ssid`: A &str representing the SSID to connect to.
+    /// - `password`: An Option that may contain the password of the SSID.
+    /// 
+    /// # Returns
+    /// 
+    /// A `Result` with Ok if the operation completed successfully, or an `WifiError` if it fails.
+    ///
+    /// # Errors
+    ///
+    /// - `WifiError::ConfigurationError`: If the configuration of the wifi driver fails.
+    /// - `WifiError::StartingError`: Error while starting wifi driver.
+    /// - `WifiError::ConnectingError`: Error while connecting to wifi.
     pub async fn connect(&mut self, ssid: &str, password: Option<String>) -> Result<(), WifiError> {
         let mut wifi_pass = "".to_string();
 
@@ -58,34 +78,67 @@ impl <'a>WifiDriver<'a> {
         self.controller.set_configuration(&wifi_configuration).map_err(|_| WifiError::ConfigurationError)?;
 
         self.controller.start().await.map_err(|_| WifiError::StartingError)?;
-        //TODO: Delete this!
-        println!("DEBUG:Wifi started");
     
         self.controller.connect().await.map_err(|_| WifiError::ConnectingError)?;
-        //TODO: Delete this!
-        println!("DEBUG:wifi connected");
     
         self.controller.wait_netif_up().await.map_err(|_| WifiError::ConnectingError)?;
-        //TODO: Delete this!
-        println!("DEBUG:wifi netif up"); 
 
         Ok(())
     }
 
+    /// Checks if the driver is already started.
+    /// 
+    /// # Returns
+    /// 
+    /// A Result containing a bool that indicated whether the driver has started or not. Else, 
+    /// a `WifiError`.
+    /// 
+    /// # Errors
+    /// 
+    /// - `WifiError::WifiNotInitialized`: If WiFi is not initialized by esp_wifi_init.
     pub fn is_started(&self) -> Result<bool, WifiError> {
-        self.controller.is_started().map_err(|_| WifiError::WifiNotInitialized) // This error appears if WiFi is not initialized by esp_wifi_init
+        self.controller.is_started().map_err(|_| WifiError::WifiNotInitialized)
     }
 
+    /// Checks if the driver is already connectedto a wifi network.
+    /// 
+    /// # Returns
+    /// 
+    /// A Result containing a bool that indicated whether the driver has connected or not. Else, 
+    /// a `WifiError`.
+    /// 
+    /// # Errors
+    /// 
+    /// - `WifiError::WifiNotInitialized`: If WiFi is not initialized by esp_wifi_init.
     pub fn is_connected(&self) -> Result<bool, WifiError> {
-        self.controller.is_connected().map_err(|_| WifiError::WifiNotInitialized) // This error appears if WiFi is not initialized by esp_wifi_init
+        self.controller.is_connected().map_err(|_| WifiError::WifiNotInitialized)
     }
-
+    
+    /// Get the ip address of the device.
+    /// 
+    /// # Returns
+    /// 
+    /// A Result containing a Ipv4Addr with the device ip address or a `WifiError` in case of failure.
+    /// 
+    /// # Errors
+    /// 
+    /// - `WifiError::InformationError`: If WiFi driver can not get its own ip.
     pub fn get_address_info(&self) -> Result<Ipv4Addr, WifiError> {
         let netif = self.controller.wifi().sta_netif();
         let info = netif.get_ip_info().map_err(|_| WifiError::InformationError)?;
         Ok(info.ip)
     }
-
+    
+    /// Gets the DNS ip address.
+    /// 
+    /// # Returns
+    /// 
+    /// A Results containing an Ipv4Addr or a `WifiError` if if fails.
+    /// 
+    /// # Errors
+    /// 
+    /// - `WifiError::InformationError`: If getting the informatiof of the netif fails.
+    /// - `WifiError::DnsNotFound`: If the netif info does not have the dns ip address.
     pub fn get_dns_info(&self) -> Result<Ipv4Addr, WifiError> {
         let netif = self.controller.wifi().sta_netif();
         let info = netif.get_ip_info().map_err(|_| WifiError::InformationError)?;
@@ -95,10 +148,28 @@ impl <'a>WifiDriver<'a> {
         }
     }
 
+    /// Creates a new HttpClient ready to use.
+    /// 
+    /// # Returns
+    /// 
+    /// A Result containing the new HttpClient or a `WifiError` if the inizialization fails.
+    /// 
+    /// # Errors
+    /// 
+    /// - `WifiError::HttpError`: If the inizialization of the HttpClient fails.
     pub fn get_http_client(&self) -> Result<HttpClient, WifiError> {
         HttpClient::new().map_err(|_| WifiError::HttpError)
     }
-
+    
+    /// Creates a new HttpsClient ready to use.
+    /// 
+    /// # Returns
+    /// 
+    /// A Result containing the new HttpsClient or a `WifiError` if the inizialization fails.
+    /// 
+    /// # Errors
+    /// 
+    /// - `WifiError::HttpError`: If the inizialization of the HttpsClient fails.
     pub fn get_https_client(&self) -> Result<HttpsClient, WifiError> {
         HttpsClient::new().map_err(|_| WifiError::HttpError)
     }
