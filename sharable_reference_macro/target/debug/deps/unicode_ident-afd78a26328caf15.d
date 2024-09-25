76382bca28de195d431e7151c81e1284aa14a79b
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-09-24 23:08:08 -0300

    deleting macro target

--- sharable_reference_macro/target/debug/deps/unicode_ident-afd78a26328caf15.d
+++
@@ -1,6 +0,0 @@
-/home/palito/Desktop/tpp/esp32framework/sharable_reference_macro/target/debug/deps/libunicode_ident-afd78a26328caf15.rmeta: /home/palito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /home/palito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
-
-/home/palito/Desktop/tpp/esp32framework/sharable_reference_macro/target/debug/deps/unicode_ident-afd78a26328caf15.d: /home/palito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /home/palito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
-
-/home/palito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs:
-/home/palito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs:
