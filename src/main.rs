06d9c74a63495fa27f1857b00faf0a7911e18db3
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-09-27 12:26:24 -0300

    analog in example withought framework

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
-//! Example using pin GPIO5 as analog in to obtain analog messurements of the sound intensity
-//! The example takes a smooth reading during the first second, and then takes another 10 readings
-//! and compares the sound intensity with the first reading.
+use std::time::{Duration, Instant};
 
-use esp32framework::Microcontroller;
+use esp_idf_svc::{hal::{adc::{attenuation, oneshot::{config::AdcChannelConfig, AdcChannelDriver, AdcDriver}, Resolution, ADC1}, delay::FreeRtos, gpio::Gpio5, prelude::Peripherals}, sys::EspError};
 
 fn main(){
 
-    let mut micro = Microcontroller::new();
-    let mut sound_in = micro.set_pin_as_analog_in_no_atten(5);
-    micro.wait_for_updates(Some(2000));
-    let first_second = sound_in.smooth_read_during(1000).unwrap() as f32;
-    println!("Value of first second #{first_second}");
-    micro.wait_for_updates(Some(2000));
+    esp_idf_svc::sys::link_patches();
+    let peripherals = Peripherals::take().unwrap();
+    
+    let adc_driver = AdcDriver::new(peripherals.adc1).unwrap();
+    let mut config = AdcChannelConfig::new();
+    config.attenuation = attenuation::NONE;
+    config.resolution = Resolution::Resolution12Bit;
+    config.calibration = true;
+    
+    let pin = peripherals.pins.gpio5;
+    let mut sound_in = AdcChannelDriver::new(adc_driver, pin, &config).unwrap();
 
+    FreeRtos::delay_ms(2000);
+    let first_second = smooth_read_during(&mut sound_in, 1000).unwrap() as f32;
+    println!("Value of first second #{first_second}");
+    FreeRtos::delay_ms(2000);
+    
     for _ in 0..10{
-        let sound = sound_in.smooth_read_during(1000).unwrap() as f32;
+        let sound = smooth_read_during(&mut sound_in, 1000).unwrap() as f32;
         let louder = (sound - first_second)* 100.0 /first_second;
-        println!("sound in is #{louder} % louder than first second" );
-        micro.wait_for_updates(Some(100));
+        println!("sound in  is #{louder} % louder than first second" );
+        FreeRtos::delay_ms(100);
     }
     println!("\n End of example");
-    micro.wait_for_updates(None);
+    FreeRtos::delay_ms(u32::MAX);
+}
+
+pub fn smooth_read_during(adc_driver : &mut AdcChannelDriver<'static, Gpio5, AdcDriver<'static, ADC1>>, ms: u16)-> Result<u16, EspError>{
+    let mut smooth_val: u64 = 0;
+    let duration = Duration::from_millis(ms as u64);
+    let starting_time  = Instant::now();
+    let mut amount_of_samples = 0;
+    while starting_time.elapsed() < duration{
+        let read_val = adc_driver.read()?;
+        smooth_val += read_val as u64;
+        amount_of_samples += 1;
+    }
+    let result = smooth_val / amount_of_samples as u64;
+    Ok(result as u16)
 }
