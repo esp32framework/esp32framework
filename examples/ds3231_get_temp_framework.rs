1d523d8eb60128e0b313a5b7a532c2ad71baf3fb
DiegoCivi <dcivini@fi.uba.ar>
2024-09-28 17:17:24 -0300

    Solve DS3231 TODOs. Sensor library now has 12-hour mode or 24-hour mode

--- examples/ds3231_get_temp_framework.rs
+++ examples/ds3231_get_temp_framework.rs
@@ -1,12 +1,12 @@
 //! Example using pin GPIO5 (sda) and GPIO6 (scl) with i2c to communicate
 //! with a ds3231 sensor. Then it will ask the sensor temperature and print it every second.
 
-use esp32framework::{Microcontroller, sensors::DS3231};
+use esp32framework::{sensors::{HourMode, DS3231}, Microcontroller};
 
 fn main() {
     let mut micro = Microcontroller::new();
     let i2c = micro.set_pins_for_i2c_master(5,6);
-    let mut ds3231 = DS3231::new(i2c);
+    let mut ds3231 = DS3231::new(i2c, HourMode::TwentyFourHour);
 
     loop {
         let temp = ds3231.get_temperature();
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use esp32framework::Microcontroller;
+//! Example using pin GPIO5 (sda) and GPIO6 (scl) with i2c to set 
+//! a date and time in a ds3231 sensor. Then it will ask the sensor
+//! for the time and print it in the screen.
 
-fn main(){
+use esp32framework::{sensors::{DateTime, HourMode, DS3231}, serial::READER, Microcontroller};
+
+fn main() {
 
     let mut micro = Microcontroller::new();
-    let mut client = micro.ble_client();
-    client.connect_to_device_with_service(None, &esp32framework::ble::BleId::FromUuid16(0x5678)).unwrap();
-   
-    micro.wait_for_updates(None);
+    let i2c = micro.set_pins_for_i2c_master(5,6);
+    let mut ds3231 = DS3231::new(i2c, HourMode::TwelveHourPM);
+
+    let date_time = DateTime {
+        second: 50,
+        minute: 59,
+        hour: 12,
+        week_day: 4,
+        date: 24,
+        month: 7,
+        year: 24,
+    };
+
+    ds3231.set_time(date_time).unwrap();
+    
+    loop {
+        // Set reading address in zero to read seconds, minutes, hours, day, day number, month and year
+        let date_time = ds3231.read_and_parse();
+
+        println!("{}, {}/{}/20{}, {:02}:{:02}:{:02}", date_time["dow"], date_time["day_number"],
+                                                      date_time["month"], date_time["year"], date_time["hrs"], 
+                                                      date_time["min"], date_time["secs"]);
+
+        let meridiem = ds3231.meridiem().unwrap();
+
+        println!("{:?}", meridiem);
+
+        micro.sleep(1000);
+    }
 }
