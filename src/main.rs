907376720d89dd0d8a756b71547e7dbfda0796b3
Joaquin <jorivero@fi.uba.ar>
2024-10-08 01:27:35 -0300

    testing manually

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,16 @@ fn main() {
     let mut ds3231: DS3231<'_> = DS3231::new(i2c);
 
     loop{
-        // let data = ds3231.show_data("hrs").unwrap();
-        let sum = ds3231.read_n_times_and_sum("secs".to_string(), 4, 1000).unwrap();
-        println!("four seconds sum is: {}", sum );
+        ds3231.show_data("secs".to_string()).unwrap();
+        // let data = ds3231.read_n_times_and_sum("secs".to_string(), 4, 1000).unwrap();
+        // let data = ds3231.read_n_times_and_avg("secs".to_string(), 4, 1000).unwrap();
+
+        let data = ds3231.read_n_times_and_aggregate("secs".to_string(), 4, 1000, |data| {
+            println!("{:?}", data)
+        }).unwrap();
+        
+        // ds3231.show_data("secs".to_string()).unwrap();
+        // println!("data: {}", data);
 
         micro.sleep(500);
     }
