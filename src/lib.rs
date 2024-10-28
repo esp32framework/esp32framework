994c5684a533a8324472420033edaf816471e385
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-10-27 23:45:24 -0300

    further documentation

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,15 @@ mod esp_test_runner;
 
 /// The esp_test module, provides a simple way to have a test framework that runs on the microcontroller.
 /// After each test the microcontoller will be restarted to guarantee the independence between tests.
+/// 
+/// In order to use the custom test runner make sure to include the folowing 3 lines at the beggining of 
+/// yout main or lib file. And then call the `esp_test::use_esp32_tests` macro
+/// ```
+/// #![feature(custom_test_frameworks)]
+/// #![feature(test)]
+/// #![test_runner(test_runner_mod::esp_test_runner)]
+/// ```
+/// 
 pub mod esp_test{
     pub use super::esp_test_runner::*;
     pub use esp32_testing_macro::*;
