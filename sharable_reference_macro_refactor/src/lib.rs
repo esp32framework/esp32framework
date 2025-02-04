08a0f5d19b0a75b81358ede7827b7a0983d2b373
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2025-02-04 15:49:22 -0300

    filtering ImplItemFn

--- sharable_reference_macro_refactor/src/lib.rs
+++ sharable_reference_macro_refactor/src/lib.rs
@@ -1,4 +1,4 @@
-use syn::{Ident, ImplItem, ImplItemFn, ItemImpl};
+use syn::{FnArg, Ident, ImplItem, ImplItemFn, ItemImpl};
 
 #[derive(Debug)]
 enum SharableReferenceMacroError{
@@ -6,8 +6,32 @@ enum SharableReferenceMacroError{
     ExpectedSegmentInTypePath
 }
 
+trait SharableReferenceImplItemFnExt{
+    fn modify_for_sharable_ref(&mut self);
+    /// Returns wheter or not the ImplItemFn is public
+    fn is_public(&self)-> bool;
+    /// Returns wheter or not the ImplItemFn receives a reference to self, mutable or not
+    fn receives_reference_to_self(&self)->bool;
+    /// Returns whether or not this ImplItemFn should be filtered or not
+    fn filter_for_sharable_ref(&self)->bool{
+        !self.is_public() || !self.receives_reference_to_self()
+    }
+}
+
 trait SharableReferenceImplBlockExt {
+    /// Modifies impl block name by removing first character.
+    /// 
+    /// # Returns
+    ///
+    /// `Ok(())` uppon sucess, otherwise a `SharableReferenceMacroError`.
+    ///
+    /// # Errors
+    ///
+    /// - `SharableReferenceMacroError::ExpectedTypePath`: Upon invalid type.
+    /// - `SharableReferenceMacroError::ExpectedSegmentInTypePath`: If there are no segments in path.
     fn remove_first_character(&mut self)-> Result<(), SharableReferenceMacroError>;
+    /// Filters and modifies any item of the impl block that needs changing
+    fn modify_and_filter_items(&mut self);
 }
 
 impl SharableReferenceImplBlockExt for ItemImpl{
@@ -22,5 +46,54 @@ impl SharableReferenceImplBlockExt for ItemImpl{
             _ => Err(SharableReferenceMacroError::ExpectedTypePath),
         }
     }
+
+    fn modify_and_filter_items(&mut self) {
+        self.items.retain_mut(|item|
+            match item {
+                ImplItem::Fn(impl_item_fn) => {
+                    impl_item_fn.modify_for_sharable_ref();
+                    !impl_item_fn.filter_for_sharable_ref()
+                },
+                _ => true,
+            }
+        );
+    }
 }
 
+impl SharableReferenceImplItemFnExt for ImplItemFn{
+    fn modify_for_sharable_ref(&mut self) {
+    }
+    
+    fn is_public(&self)-> bool {
+        match self.vis{
+            syn::Visibility::Inherited => false,
+            _ => true
+        }
+    }
+    
+    fn receives_reference_to_self(&self)->bool {
+        self.sig.inputs.iter().any(|arg| match arg{
+            FnArg::Receiver(receiver) => receiver.reference.is_some(),
+            FnArg::Typed(_) => false,
+        })
+    }
+}
+/*
+pub fn sharable_reference_wrapper(args: TokenStream, item: TokenStream) -> TokenStream {
+    let mut input_original = parse_macro_input!(item as ItemImpl);
+    let mut input = input_original.clone();
+    let args = parse_macro_input!(args as StringArgs);
+    println!("alo");
+    input.remove_first_character().unwrap();
+    input.modify_and_filter_items();
+
+    //println!("\n\n");
+    //println!("{}", input.to_token_stream());
+    //println!("\n\n");
+    return TokenStream::from(quote! {
+        #input_original
+        #input
+    });
+    todo()!
+}
+*/
